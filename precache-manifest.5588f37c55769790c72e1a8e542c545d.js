f67e19f88cf2af02c6e160c5ee07688d9c6b0d25
VinayRana307 <vinay6365355@gmail.com>
2020-09-03 10:29:34 +0530

    Updates

---
+++ precache-manifest.5588f37c55769790c72e1a8e542c545d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1f41bee1987286eb1cb8ce1523be4f",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "7c399b41020c6bb5f7e9",
+    "url": "/reactwebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e4c9be10aa3a1845040",
+    "url": "/reactwebsite/static/js/2.eb94f331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwebsite/static/js/2.eb94f331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c399b41020c6bb5f7e9",
+    "url": "/reactwebsite/static/js/main.777b2ebf.chunk.js"
+  },
+  {
+    "revision": "eec87808e158f3f4700a",
+    "url": "/reactwebsite/static/js/runtime-main.824e2a87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
